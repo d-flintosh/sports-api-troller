d41d1ba9a8c7293c7e9062e0d37d6eb467598833
Drew Flintosh <>
2021-06-15 12:30:58 -0500

    adding a few more schools

--- src/models/Schools.py
+++ src/models/Schools.py
@@ -5,13 +5,17 @@ from src.models.Emojis import Emojis
 COLLEGES_TO_RUN = {
     'arizona state': 'azst',
     'connecticut': 'connecticut',
+    'duke': 'duke',
+    'florida': 'florida',
     'fsu': 'fsu',
     'florida state': 'fsu',
     'florida state university': 'fsu',
     'illinois': 'illinois',
     'miami': 'miami',
     'michigan': 'michigan',
+    'north carolina': 'northcarolina',
     'notre dame': 'notredame',
+    'virginia': 'virginia',
     'wisconsin': 'wisconsin'
 }
 
@@ -32,6 +36,22 @@ class Schools(Enum):
         'basketball': {
             'header': f'{Emojis.BASKETBALL.value} UConn {Emojis.BASKETBALL.value}\n'
         }
+    }
+    duke = {
+        'baseball': {
+            'header': f'{Emojis.BASEBALL.value} Duke {Emojis.BASEBALL.value}\n'
+        },
+        'basketball': {
+            'header': f'{Emojis.BASKETBALL.value} Duke {Emojis.BASKETBALL.value}\n'
+        }
+    }
+    florida = {
+       'baseball': {
+           'header': f'{Emojis.BASEBALL.value} Florida {Emojis.BASEBALL.value}\n'
+       },
+       'basketball': {
+           'header': f'{Emojis.BASKETBALL.value} Florida {Emojis.BASKETBALL.value}\n'
+       }
     }
     fsu = {
        'baseball': {
@@ -65,12 +85,28 @@ class Schools(Enum):
             'header': f'{Emojis.BASKETBALL.value} @umichbball {Emojis.BASKETBALL.value}\n'
         }
     }
+    northcarolina = {
+        'baseball': {
+            'header': f'{Emojis.BASEBALL.value} UNC {Emojis.BASEBALL.value}\n'
+        },
+        'basketball': {
+            'header': f'{Emojis.BASKETBALL.value} UNC {Emojis.BASKETBALL.value}\n'
+        }
+    }
     notredame = {
         'baseball': {
             'header': f'{Emojis.BASEBALL.value} Notre Dame {Emojis.BASEBALL.value}\n'
         },
         'basketball': {
             'header': f'{Emojis.BASKETBALL.value} Notre Dame {Emojis.BASKETBALL.value}\n'
+        }
+    }
+    virginia = {
+        'baseball': {
+            'header': f'{Emojis.BASEBALL.value} Virginia {Emojis.BASEBALL.value}\n'
+        },
+        'basketball': {
+            'header': f'{Emojis.BASKETBALL.value} Virginia {Emojis.BASKETBALL.value}\n'
         }
     }
     wisconsin = {
